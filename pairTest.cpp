2df20853c1fac9b555bb05f14ad650b96158608d
Omar Alispahic <omaralispahic@Omars-MacBook-Pro.local>
2025-05-29 19:18:46 +0200

    Added examples for flip method

--- pairTest.cpp
+++ pairTest.cpp
@@ -8,16 +8,22 @@ int main() {
     pair combinedPair = pair("Initialised value", 2);
 
     std::cout << combinedPair; // should print (Initialised value, 2)
-
+    pair dobarpar(1,2);
     combinedPair.modify("Modifies the value",0);
     NR();
     std::cout << combinedPair; // now prints (Modifies the value, 2)
     NR();
     std::cout<<combinedPair.component();
     NR();
-    std::cout<<combinedPair.component(0);
+    std::cout<<combinedPair.component();
     NR();
-    std::cout<<combinedPair.component(1.f)<<" Molim???"; //still working on being able to acces each component
+    std::cout<<combinedPair.component()<<" Molim???"; //still working on being able to acces each component
+    combinedPair.modify(1,1);
+    NR();
+    combinedPair.component() = "Trying this "; //you can change the values of the components too
+    std::cout<<combinedPair.component();
+    auto flipped = combinedPair.flip(); // can flip the values but returns a new object
+    std::cout<<flipped;
 
 
     return 0;
